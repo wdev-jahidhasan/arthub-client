872b45446b859656e475ded686265b6a18f7f9a3
wdev-jahidhasan <wdev.jahidhasan@gmail.com>
2026-08-23 19:43:40 +0600

    mongodb and betterauth setup

---
+++ src/lib/auth.js
@@ -0,0 +1,15 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db("artHub");
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db, {
+    client
+  }),
+   emailAndPassword: { 
+    enabled: true, 
+  },
+});
